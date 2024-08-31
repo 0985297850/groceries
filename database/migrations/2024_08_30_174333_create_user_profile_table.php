3e52b6e9d575d14b57f13f7e7b0ef083a8c5a014
phong_nd <dinhphong22082000@gmail.com>
2024-08-31 16:59:24 +0700

    push

--- database/migrations/2024_08_30_174333_create_user_profile_table.php
+++ database/migrations/2024_08_30_174333_create_user_profile_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('last_name');
             $table->string('first_name');
             $table->string('avatar')->nullable();
-            $table->enum('gender', ['Nam', 'Nữ', 'Khác']);
+            $table->enum('gender', ['male', 'female', 'other']);
             $table->string('phone');
             $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
