7bbb5ca8fb2dbbb6667e10032aea26ccf06b78f3
phong_nd <dinhphong22082000@gmail.com>
2024-09-04 08:17:46 +0700

    push fix regex phone number

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -45,7 +45,7 @@ class AuthController extends Controller
                 'user_name' => 'required|string|between:2,100',
                 'email' => 'required|string|email|max:100|unique:users',
                 'password' => 'required|string|min:6',
-                'phone' => 'required|numeric|between:10,11',
+                'phone' => 'required|regex:/^0[0-9]{9,10}$/',
             ]);
 
             if ($validator->fails()) {
